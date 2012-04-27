b1ca55133b8e569ca86642ce656d84af989b3ac6
Larmon <larmon.luo@gmail.com>
2012-04-27 19:23:35 -0400

    changed some print statements in letter recognition script

--- letter_recognition/script.sh
+++ letter_recognition/script.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env/bash
 
-for rate in {1..4}; do
+for rate in {1..9}; do
   for hidden in {1..30}; do
-    echo $rate
-    echo $hidden
-    #python nn.py --rounds=10 --learning_rate=.$rate --hidden=$hidden >> output2.txt
+    python nn.py --rounds=10 --learning_rate=.$rate --hidden=$hidden >> output2.txt
     echo -e "\n \n" >> output2.txt
   done
 done
