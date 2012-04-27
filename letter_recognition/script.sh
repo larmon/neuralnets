21809abdffc1e1edb90a67d9b2306b912e7e8052
Larmon <larmon.luo@gmail.com>
2012-04-27 19:24:55 -0400

    changed pathway for letter recognition script

--- letter_recognition/script.sh
+++ letter_recognition/script.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env/bash
+#!/usr/bin/env bash
 
 for rate in {1..9}; do
   for hidden in {1..30}; do
-    python nn.py --rounds=10 --learning_rate=.$rate --hidden=$hidden >> output2.txt
-    echo -e "\n \n" >> output2.txt
+    python nn.py --rounds=10 --learning_rate=.$rate --hidden=$hidden >> letterout.txt
+    echo -e "\n \n" >> letterout.txt
   done
 done
 
